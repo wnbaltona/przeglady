93771c2081a4b907c760d70f5bf00a4c969593de
wnbaltona <wniziolek@baltona.pl>
2026-08-26 13:14:49 +0200

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,6 @@
 window.APP_CONFIG={
   supabaseUrl:'https://bsisclhysmvzqgggnpna.supabase.co',
   supabaseAnonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzaXNjbGh5c212enFnZ2ducG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNDEwNjQsImV4cCI6MjEwMDcxNzA2NH0._ZcGGj0FTRf1NsO1WvVXVchwwozaaaeaPUKKKRi1hcM',
-  allowRegistration:false
+  allowRegistration:false,
+  inactivityHours:24
 };
