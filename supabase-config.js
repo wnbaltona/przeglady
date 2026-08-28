43c206eae3e7ef6af571c7bbb1e9d4192a8b0ef1
wnbaltona <wniziolek@baltona.pl>
2026-08-28 10:00:59 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,7 @@ window.APP_CONFIG={
   supabaseUrl:'https://bsisclhysmvzqgggnpna.supabase.co',
   supabaseAnonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzaXNjbGh5c212enFnZ2ducG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNDEwNjQsImV4cCI6MjEwMDcxNzA2NH0._ZcGGj0FTRf1NsO1WvVXVchwwozaaaeaPUKKKRi1hcM',
   allowRegistration:false,
-  inactivityHours:3,
+  inactivityHours:24,
   // Wklej tutaj PUBLICZNY klucz VAPID wygenerowany przez GENERUJ_KLUCZE_PUSH.ps1.
   // Klucza prywatnego nigdy nie wpisuj do tego pliku ani do GitHub.
   pushVapidPublicKey:'BDjVwnvDKvuTQS7S4DXoP4S259Xd0ILlTvI_3QR7f0OSmwnmR2Ajkd-gslpiQZ8T28Sias3gE7pFaTWtWjwdyXI'
